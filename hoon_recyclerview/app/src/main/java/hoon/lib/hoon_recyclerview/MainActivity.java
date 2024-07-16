26bd48b84732bbfc96d88cf0a3280550a0f7f54a
boris920308 <jhoonj0308@gmail.com>
2024-07-16 20:11:02 +0900

    [feat] viewbinding 추가

--- hoon_recyclerview/app/src/main/java/hoon/lib/hoon_recyclerview/MainActivity.java
+++ hoon_recyclerview/app/src/main/java/hoon/lib/hoon_recyclerview/MainActivity.java
@@ -8,12 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import hoon.lib.hoon_recyclerview.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.tvTitle.setText("hello view binding");
     }
 }
