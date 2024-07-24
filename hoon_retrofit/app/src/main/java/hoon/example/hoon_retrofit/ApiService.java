fb9a3086a892ccfa60dfc79a9ef32496953e8c75
boris920308 <jhoonj0308@gmail.com>
2024-07-24 17:42:57 +0900

    [feat] rxjava 적용

--- hoon_retrofit/app/src/main/java/hoon/example/hoon_retrofit/ApiService.java
+++ hoon_retrofit/app/src/main/java/hoon/example/hoon_retrofit/ApiService.java
@@ -1,10 +1,11 @@
 package hoon.example.hoon_retrofit;
 
+import io.reactivex.Single;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface ApiService {
     @GET("users/{userId}")
-    Call<User> getUser(@Path("userId") int userId);
+    Single<User> getUser(@Path("userId") int userId);
 }
--- hoon_retrofit/app/src/main/java/hoon/example/hoon_retrofit/MainActivity.java
+++ hoon_retrofit/app/src/main/java/hoon/example/hoon_retrofit/MainActivity.java
@@ -10,14 +10,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import hoon.example.hoon_retrofit.databinding.ActivityMainBinding;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
+
+    private ApiService apiService;
+    private CompositeDisposable compositeDisposable = new CompositeDisposable(); //rxjava 구독관리
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,31 +35,36 @@ public class MainActivity extends AppCompatActivity {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
-        ApiService apiService = retrofit.create(ApiService.class);
+        apiService = retrofit.create(ApiService.class);
 
         binding.btnGet.setOnClickListener(view -> {
                     binding.tvData.setText("click btn, loading...");
-                    Call<User> call = apiService.getUser(1);
-                    call.enqueue(new Callback<User>() {
-                        @Override
-                        public void onResponse(Call<User> call1, Response<User> response) {
-                            if (response.isSuccessful()) {
-                                User user = response.body();
-                                binding.tvData.setText("onResponse, response.isSuccessful(), user = " + user.getUserName());
-                            } else {
-                                binding.tvData.setText("onResponse, response.isSuccessful(), request fail");
-                            }
-                        }
 
-                        @Override
-                        public void onFailure(Call<User> call1, Throwable t) {
-                            binding.tvData.setText("onFailure");
-                        }
-                    });
+                    compositeDisposable.add(
+                            apiService.getUser(1)
+                                    .subscribeOn(Schedulers.io())
+                                    .observeOn(AndroidSchedulers.mainThread())
+                                    .subscribe(
+                                            user -> {
+                                                // success
+                                                binding.tvData.setText("success, user.name = " + user.getName());
+                                            },
+                                            throwable -> {
+                                                // fail
+                                                binding.tvData.setText("fail");
+                                            }
+                                    )
+                    );
                 }
         );
     }
 
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        compositeDisposable.clear();
+    }
 }
