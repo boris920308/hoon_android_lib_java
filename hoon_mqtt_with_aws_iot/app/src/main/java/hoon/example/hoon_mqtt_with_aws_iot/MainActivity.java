fde29671bb62f4c247a279b4987c3a4eaf9bbf14
boris920308 <jhoonj0308@gmail.com>
2024-07-30 10:37:08 +0900

    [init] init mqtt with aws iot project

---
+++ hoon_mqtt_with_aws_iot/app/src/main/java/hoon/example/hoon_mqtt_with_aws_iot/MainActivity.java
@@ -0,0 +1,22 @@
+package hoon.example.hoon_mqtt_with_aws_iot;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import hoon.example.hoon_mqtt_with_aws_iot.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
