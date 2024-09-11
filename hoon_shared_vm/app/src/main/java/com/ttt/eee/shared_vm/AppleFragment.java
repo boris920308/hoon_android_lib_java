3a5d975cbe3313f47961b47b49fb61bfc6964b65
boris920308 <jhoonj0308@gmail.com>
2024-09-11 15:12:24 +0900

    feat. fragment 추가
    연결작업은 추가작업 필요

---
+++ hoon_shared_vm/app/src/main/java/com/ttt/eee/shared_vm/AppleFragment.java
@@ -0,0 +1,29 @@
+package com.ttt.eee.shared_vm;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.ttt.eee.shared_vm.databinding.FragmentAppleBinding;
+
+public class AppleFragment extends Fragment {
+
+    private FragmentAppleBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentAppleBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
