5c8e45b5027a6b9075334d4507a4f59e982a0cab
boris920308 <jhoonj0308@gmail.com>
2024-09-11 14:30:59 +0900

    init. init shared vm

---
+++ hoon_shared_vm/app/src/main/java/com/ttt/eee/shared_vm/MainActivity.java
@@ -0,0 +1,24 @@
+package com.ttt.eee.shared_vm;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.ttt.eee.shared_vm.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
